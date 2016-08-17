0194c92497781475d836bcd79b6a55efac518b06
thathwar <thrivikramhathwar@gmail.com>
2016-08-17 20:53:01 +0300

    Exchange Controller

---
+++ EXCHNG/EXCHNG.ExchngWebApp/Models/Cube.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EXCHNG.ExchngWebApp.Models
+{
+    public class Cube
+    {
+        public DateTime Date { get; set; }
+
+        public List<Rate> Rates { get; set; }   
+    }
+
+    public class Rate
+    {
+        public decimal Value { get; set; }
+
+        public string Currency { get; set; }    
+    }
+}
